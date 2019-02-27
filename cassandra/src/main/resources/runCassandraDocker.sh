c3b31f51ee8e0c4ab73acef0b0037362cd53a6b5
Yannnick Bruyninckx <yannick.bruyninckx@hermes-ecs.com>
2019-02-27 20:20:36 +0100

    Adding connection info on docker db

--- cassandra/src/main/resources/runCassandraDocker.sh
+++ cassandra/src/main/resources/runCassandraDocker.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 docker run -p 7000:7000 -p 9042:9042 -d --name my-cassandra cassandra:2.1.20
+
+### connect to db insance
+# docker exec -it my-cassandra /bin/bash
+
+### use cql
+# cqlsh
